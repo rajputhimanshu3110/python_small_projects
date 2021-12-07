7936fdec550345e35046090e5f514d86f45a0769
Thakur Viraj Singh <75830024+x-lucifur@users.noreply.github.com>
2021-12-07 19:20:03 +0530

    Update guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -1,8 +1,9 @@
 import random
 print("\t*****WELCOME TO NUMBER GUESSING GAME*****\n")
 list = [93,52,55,165,28,17,65,954.326,454]
-n = random.choice(list)
+n = random.randrange(1,500)
 chances = 9
+print("Enter number b/w 1 to 500")
 while (True):
     if(chances>0):
         number = int(input("Enter number : "))
